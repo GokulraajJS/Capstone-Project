dac3c92a62fde8159b6f7c1a3e3814204dc4be3e
Ubuntu <ubuntu@ip-172-31-18-172.ec2.internal>
2024-08-08 07:13:42 +0000

    Source files and other files are committed

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 
 echo "$DockerHub_password" | docker login --username "$DockerHub_username" --password-stdin
 
-docker tag $Docker_localimgname $DockerHub_username/$Docker_tag
+docker tag $Docker_localimgname $DockerHub_username/$Docker_prod_tag
 
-docker push $DockerHub_username/$Docker_tag
+docker push $DockerHub_username/$Docker_prod_tag
