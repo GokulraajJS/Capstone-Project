099cdef0a3a2fa37a704c754b57104da90d7ee17
Ubuntu <ubuntu@ip-172-31-18-172.ec2.internal>
2024-08-12 11:37:40 +0000

    Rearrangement and Jenkinsfile added

---
+++ build/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build -t $Docker_localimgname .
+
---
+++ build/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+echo "$DockerHub_password" | docker login --username "$DockerHub_username" --password-stdin
+
+docker tag $Docker_localimgname $DockerHub_username/$Docker_tag
+
+docker push $DockerHub_username/$Docker_tag
